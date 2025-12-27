c7f5c23295bbc073558fadd1f6c27318d8168c48
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-27 14:59:42 +0530

    File is change

---
+++ JS_Class/Objects.js
@@ -0,0 +1,13 @@
+var cap = {
+    firstName: 'Steve',
+    lastName: 'Rogers',
+    age: 102,
+    friends: ['Bucky Barnes', 'Bruce Banner', 'Tony Stark'],
+    address: {
+        state: 'New York',
+        city: {
+            name: 'Brooklyn',
+            pincode: 123456
+        }
+    }
+}
