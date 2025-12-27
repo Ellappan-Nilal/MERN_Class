23998f14763e750f63c2054d00b195c5944e4261
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-27 14:25:44 +0530

    File is addd

--- JS_Class/script.js
+++ JS_Class/script.js
@@ -1,6 +1,15 @@
-console.log("This is the script.js file");
+//console.log("This is the script.js file")
 
-let a=10;
-let b=20;
+//let a=10
+//let b=20
 
-console.log("Sum:", a + b);
+//console.log("Sum:", a + b)
+
+//var greet='hello world'
+//console.log(greet)
+
+
+//var arr=[1,"JavaScript",true,45.6]
+//console.log("Array:", arr)
+
+//Objects
